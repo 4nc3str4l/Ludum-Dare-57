edd91834e332ddd64815faeb21739db305ffc343
4nc3str4l <cmuri_10@hotmail.com>
2025-04-05 13:07:02 +0200

    Use some acceleration for grappling

--- Assets/Scripts/Grappling/GrapplingStamina.cs
+++ Assets/Scripts/Grappling/GrapplingStamina.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 
 public class GrapplingStamina : MonoBehaviour
 {
-    public float WhileGrapplingFactor = -1;
-    public float WhileNotGrapplingFactor = 2;
-
-    public float MaxStamina = 100;
-    public float CurrentStamina = 100;
-
-    private float m_LastFrameStamina = 100;
+    [Header("Stamina Factors")]
+    public float BaseWhileGrapplingFactor = -1f;     
+    public float BaseWhileNotGrapplingFactor = 2f;   
     
+    [Header("Acceleration Settings")]
+    public float MaxAccelerationMultiplier = 3f;     
+    public float AccelerationRate = 0.5f;            
+    
+    [Header("Stamina Settings")]
+    public float MaxStamina = 100f;
+    public float CurrentStamina = 100f;
+
+    private float m_LastFrameStamina = 100f;
     private bool m_IsGrappling = false;
+    
+
+    private float m_CurrentAcceleration = 1f;   
+    private float m_TimeInCurrentState = 0f;  
     
     public delegate void OnStaminaChangedHandler(float stamina, float ratio);
     public static event OnStaminaChangedHandler OnStaminaChanged;
@@ -29,6 +38,8 @@ public class GrapplingStamina : MonoBehaviour
         HasSomeStamina = true;
         CurrentStamina = MaxStamina;
         m_LastFrameStamina = CurrentStamina;
+        m_CurrentAcceleration = 1f;
+        m_TimeInCurrentState = 0f;
     }
 
     private void OnEnable()
@@ -41,19 +52,25 @@ public class GrapplingStamina : MonoBehaviour
         GrapplingGun.OnGrappling -= GrapplingGunOnOnGrappling;
     }
 
-    private void GrapplingGunOnOnGrappling(bool isgrappling)
+    private void GrapplingGunOnOnGrappling(bool isGrappling)
     {
-        m_IsGrappling = isgrappling;
+        if (m_IsGrappling != isGrappling)
+        {
+            m_IsGrappling = isGrappling;
+            m_CurrentAcceleration = 1f;
+            m_TimeInCurrentState = 0f;
+        }
     }
     
     private void Update()
     {
-        float staminaDelta = WhileNotGrapplingFactor;
+        m_TimeInCurrentState += Time.deltaTime;
         
-        if (m_IsGrappling)
-        {
-            staminaDelta = WhileGrapplingFactor;
-        }    
+        m_CurrentAcceleration = Mathf.Min(1f + (m_TimeInCurrentState * AccelerationRate), MaxAccelerationMultiplier);
+        
+        float baseStaminaDelta = m_IsGrappling ? BaseWhileGrapplingFactor : BaseWhileNotGrapplingFactor;
+        
+        float staminaDelta = baseStaminaDelta * m_CurrentAcceleration;
         
         CurrentStamina += staminaDelta * Time.deltaTime;
 
@@ -64,12 +81,14 @@ public class GrapplingStamina : MonoBehaviour
                 CurrentStamina = MaxStamina;
                 OnStaminaAtMax?.Invoke();
             }
+            
             if (CurrentStamina <= 0)
             {
                 CurrentStamina = 0;
                 OnStaminaEmpty?.Invoke();
             }
-            if (Math.Abs(CurrentStamina - m_LastFrameStamina) < 1)
+            
+            if (Math.Abs(CurrentStamina - m_LastFrameStamina) > 0.01f)
             {
                 OnStaminaChanged?.Invoke(CurrentStamina, CurrentStamina / MaxStamina);
             }
@@ -77,5 +96,10 @@ public class GrapplingStamina : MonoBehaviour
 
         HasSomeStamina = CurrentStamina > 0.0f;
         m_LastFrameStamina = CurrentStamina;
+    }
+    
+    public float GetCurrentAccelerationMultiplier()
+    {
+        return m_CurrentAcceleration;
     }
 }
