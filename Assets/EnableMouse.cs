99320a115970c4186d8bcaf9ece400f10cf4f2e3
4nc3str4l <cmuri_10@hotmail.com>
2025-04-06 18:03:52 +0200

    Code reorganization

---
+++ Assets/EnableMouse.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EnableMouse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Enable cursor and unlock it
+        
+    }
+
+}
